76ad83c440eb031391f8efd7020b77784284f4f8
Byron Zhao <byronzhao139@gmail.com>
2023-02-06 17:13:30 -0500

    created search bar using openweather API to find cities that have been searched

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React, { useState, ChangeEvent, useEffect } from 'react';
+import Forecast from './components/Forecast';
+import Search from './components/Search';
+import useForecast from './hooks/useForecast';
+import { optionType, forecastType } from './types';
+
+function App(): JSX.Element {
+	const { term, options, forecast, onInputChange, onOptionSelect, onSubmit } =
+		useForecast();
+
+	return (
+		<main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
+			{forecast ? (
+				<Forecast data={forecast} />
+			) : (
+				<Search
+					term={term}
+					options={options}
+					onInputChange={onInputChange}
+					onOptionSelect={onOptionSelect}
+					onSubmit={onSubmit}
+				/>
+			)}
+		</main>
+	);
+}
+
+export default App;
